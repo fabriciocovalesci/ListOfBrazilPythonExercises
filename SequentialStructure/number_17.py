c4301a0050c6d6e4b4a3f218f93b1a7409404158
Fabricio <fabcovalesci@gmail.com>
2020-05-12 01:06:07 -0300

    Started number 17 and number 18

---
+++ SequentialStructure/number_17.py
@@ -0,0 +1,30 @@
+"""
+[PT]
+17. Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros 
+quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 
+6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 
+ou em galões de 3,6 litros, que custam R$ 25,00.
+
+    Informe ao usuário as quantidades de tinta a serem compradas e os respectivos 
+    preços em 3 situações:
+    17.1: comprar apenas latas de 18 litros;
+    17.2: comprar apenas galões de 3,6 litros;
+    17.3: misturar latas e galões, de forma que o preço seja o menor. Acrescente 10% de folga e 
+    sempre arredonde os valores para cima, isto é, considere latas cheias.
+
+[EN]
+17. Make a program for a paint store. The program should ask for the size in meters
+squares of the area to be painted. Consider that the ink coverage is 1 liter for each
+6 square meters and that the paint is sold in 18-liter cans, which cost R $ 80.00
+or in 3.6 liter gallons, which cost R $ 25.00.
+
+    Inform the user of the quantities of ink to be purchased and the respective
+    prices in 3 situations:
+    17.1: buy only 18 liter cans;
+    17.2: buy only 3.6 liters gallons;
+    17.3: mix cans and gallons, so that the price is the lowest. Add 10% clearance and
+    always round the values ​​up, that is, consider full cans.
+"""
+
+def calc_paint_store_part2():
+    pass
---
+++ SequentialStructure/number_18.py
@@ -0,0 +1,14 @@
+"""
+[PT]
+18. Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade 
+de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo
+usando este link (em minutos).
+
+[EN]
+18. Make a program that asks for the size of a file to download (in MB) and the speed of an 
+Internet link (in Mbps), calculate and inform the approximate time to download the file using 
+this link (in minutes).
+"""
+
+def calc_time_download():
+    pass
