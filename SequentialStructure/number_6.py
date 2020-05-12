959fabc2f0fb1de178fa828e206327761d681a52
Fabricio <fabcovalesci@gmail.com>
2020-05-11 22:35:53 -0300

    finished number 6

--- SequentialStructure/number_6.py
+++ SequentialStructure/number_6.py
@@ -6,5 +6,10 @@
 6. Make a Program that asks for the radius of a circle, calculate and show its area.
 """
 
-# def calc_area():
-#     enter = int(input(""))
+def calc_area():
+    pi = 3.141592
+    enter_radius = int(input("\nRadius of a circle: "))
+    print(f'Area of circle is {float(pi*(enter_radius**2)):.2f}')
+
+
+calc_area()
