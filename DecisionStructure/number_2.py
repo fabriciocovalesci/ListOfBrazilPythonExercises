e3b1408fb96e952b0297acb9a8d8b90ae60351f5
Fabricio <fabcovalesci@gmail.com>
2020-05-18 22:10:37 -0300

    refactored number 1 and 2

--- DecisionStructure/number_2.py
+++ DecisionStructure/number_2.py
@@ -1,13 +1,16 @@
 """
+[PT]
 2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+[EN]
 2. Make a Program that asks for a value and shows on the screen whether the value is 
 positive or negative.
 
 """
 
-number = int(input("\nEnter a number: "))
-
-if number < 0:
-    print(f"\nNumber {number} is negative")
-else:
-    print(f"\nNumber {number} is positive")
+def number_is_positive_or_negative():
+    number = int(input("\nEnter a number: "))
+    if number < 0:
+        print(f"\nNumber {number} is negative")
+    else:
+        print(f"\nNumber {number} is positive")
