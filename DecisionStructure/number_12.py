c8caa8be9a96f0ca29efe89dae012cddf346483e
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-05-27 22:53:36 -0300

    add number 12

---
+++ DecisionStructure/number_12.py
@@ -0,0 +1,29 @@
+"""
+[PT]
+12. Faça um programa para o cálculo de uma folha de pagamento, sabendo que os 
+descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e
+3% para o Sindicato e que o FGTS corresponde a 11% do Salário Bruto, mas não é descontado 
+(é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os descontos. 
+O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+    Desconto do IR:
+    Salário Bruto até 900 (inclusive) - isento
+    Salário Bruto até 1500 (inclusive) - desconto de 5%
+    Salário Bruto até 2500 (inclusive) - desconto de 10%
+    Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, 
+    dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+
+[EN]
+12. Make a program for the calculation of a payroll, knowing that the
+The discounts are based on Income Tax, which depends on the gross salary (as shown in the table below) and
+3% for the Union and that the FGTS corresponds to 11% of the Gross Salary, but is not discounted
+(it is the company that deposits). The Net Salary corresponds to the Gross Salary less discounts.
+The program should ask the user for the value of their hour and the number of hours worked in the month.
+    IR discount:
+    Gross salary up to 900 (inclusive) - exempt
+    Gross Salary up to 1500 (inclusive) - 5% discount
+    Gross Salary up to 2500 (inclusive) - 10% discount
+    Gross Salary above 2500 - 20% discount Print the information on the screen,
+    arranged according to the example below. In the example the hour value is 5 and the hour amount is 220.
+
+"""
