0555ad6ab37e09266b7cea02a664e77b101eff45
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-05-27 22:02:52 -0300

    Add exercises 10 and 11

---
+++ DecisionStructure/number_10.py
@@ -0,0 +1,10 @@
+"""
+[PT]
+10. Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino 
+ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+[EN]
+10. Make a program that asks what time you study. Ask to type M-morning or V-Evening or N-Night. 
+Print the message "Good morning!", "Good afternoon!" or good night!" or "Invalid Value!", as appropriate.
+"""
+
---
+++ DecisionStructure/number_11.py
@@ -0,0 +1,30 @@
+"""
+[PT]
+11. As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver 
+o programa que calculará os reajustes.
+Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+
+    - salários até R$ 280,00 (incluindo) : aumento de 20%
+    - salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+    - salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+    - salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    - o salário antes do reajuste;
+    - o percentual de aumento aplicado;
+    - o valor do aumento;
+    - o novo salário, após o aumento.
+
+[EN]
+11. Tabajara Organizations decided to give their employees a salary increase and hired them to develop the 
+program that will calculate the adjustments.
+Make a program that receives an employee's salary and readjust it according to the following criteria, 
+based on the current salary:
+
+    - wages up to R $ 280.00 (including): 20% increase
+    - wages between R $ 280.00 and R $ 700.00: increase of 15%
+    - wages between R $ 700.00 and R $ 1500.00: 10% increase
+    - salaries from R $ 1500.00 onwards: increase of 5% After the increase is made, inform on the screen:
+    - the salary before the readjustment;
+    - the percentage of increase applied;
+    - the amount of the increase;
+    - the new salary after the increase.
+"""
