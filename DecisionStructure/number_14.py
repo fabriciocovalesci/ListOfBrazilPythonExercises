aeaf113f534e22f37b918cfd39c6edc1ebec6d4c
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-05-29 22:18:57 -0300

    add number 14

---
+++ DecisionStructure/number_14.py
@@ -0,0 +1,29 @@
+"""
+[PT]
+14. Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, 
+e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+
+  Média de Aproveitamento  Conceito
+  Entre 9.0 e 10.0        A
+  Entre 7.5 e 9.0         B
+  Entre 6.0 e 7.5         C
+  Entre 4.0 e 6.0         D
+  Entre 4.0 e zero        E
+
+O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” 
+se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+
+[EN]
+14. Make a program that reads the two partial grades obtained by a student in a course over a semester,
+and calculate your average. The attribution of concepts follows the table below:
+
+  Average Utilization Concept
+  Between 9.0 and 10.0 A
+  Between 7.5 and 9.0 B
+  Between 6.0 and 7.5 C
+  Between 4.0 and 6.0 D
+  Between 4.0 and zero E
+
+The algorithm must show on the screen the notes, the average, the corresponding concept and the message “APPROVED”
+if the concept is A, B or C or “FAILED” if the concept is D or E.
+"""
