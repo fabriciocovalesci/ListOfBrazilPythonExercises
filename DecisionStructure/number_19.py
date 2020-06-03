e26558b6d632ce0c25c0315210039c27a8dba134
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-02 21:01:05 -0300

    iniciado number 19

---
+++ DecisionStructure/number_19.py
@@ -0,0 +1,23 @@
+"""
+[PT]
+19. Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, 
+dezenas e unidades do mesmo.
+Observando os termos no plural a colocação do "e", da vírgula entre outros. 
+Exemplo:
+326 = 3 centenas, 2 dezenas e 6 unidades
+12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 
+10, 21, 11, 1, 7 e 16
+
+[EN]
+19. Make a Program that reads an integer less than 1000 and prints the number of hundreds,
+dozens and units of it.
+Observing the plural terms, the placement of the "e", the comma among others.
+Example:
+326 = 3 hundreds, 2 tens and 6 units
+12 = 1 dozen and 2 units Test with: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20,
+10, 21, 11, 1, 7 and 16
+"""
+
+numero = int(input("-> "))
+
+if numero
