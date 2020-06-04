ddbc86db9bcd5e7890c79a36584f6a66c6fc3df1
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-03 21:37:27 -0300

    add number 21 and 22

---
+++ DecisionStructure/number_21.py
@@ -0,0 +1,25 @@
+"""
+[PT]
+21. Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque 
+e depois informar quantas notas de cada valor serão fornecidas. 
+As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo 
+de 600 reais. 
+O programa não deve se preocupar com a quantidade de notas existentes na máquina.
+
+    Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, 
+    uma nota de 50, uma nota de 5 e uma nota de 1;
+    Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, 
+    uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
+
+[EN]
+21. Make a Program for an ATM. The program should ask the user for the withdrawal amount
+and then inform how many notes of each value will be provided.
+The available grades will be 1, 5, 10, 50 and 100 reais. The minimum value is 10 reais and the maximum
+600 reais.
+The program should not be concerned with the amount of banknotes on the machine.
+
+    Example 1: To withdraw the amount of 256 reais, the program provides two 100 notes,
+    a 50 note, a 5 note and a 1 note;
+    Example 2: To withdraw the amount of 399 reais, the program provides three 100 banknotes,
+    a 50 note, four 10 notes, a 5 note and four 1 notes.
+"""
---
+++ DecisionStructure/number_22.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+21. Faça um Programa que peça um número inteiro e determine se ele é par ou impar. 
+Dica: utilize o operador módulo (resto da divisão).
+
+[EN]
+21. Make a Program that asks for an integer and determines whether it is even or odd.
+Tip: use the module operator (rest of the division).
+"""
