6f1331af6abba5c52330dfb9a108a435782883a1
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-03 21:40:55 -0300

    add number 23 and 24

---
+++ DecisionStructure/number_23.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+23. Faça um Programa que peça um número e informe se o número é inteiro ou decimal. 
+Dica: utilize uma função de arredondamento.
+
+[EN]
+23. Make a program that asks for a number and inform if the number is an integer or a decimal.
+Tip: use a rounding function.
+"""
---
+++ DecisionStructure/number_24.py
@@ -0,0 +1,15 @@
+"""
+[PT]
+24. Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. 
+O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+    par ou ímpar;
+    positivo ou negativo;
+    inteiro ou decimal.
+
+[EN]
+24. Make a program that reads 2 numbers and then ask the user which operation he wants to perform.
+The result of the operation must be accompanied by a sentence that says whether the number is:
+    even or odd;
+    positive or negative;
+    integer or decimal.
+"""
