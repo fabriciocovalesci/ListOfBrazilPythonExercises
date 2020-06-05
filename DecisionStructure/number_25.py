6bb0034c220e6afe7144b5b67f600a38f5477a9f
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-04 22:01:00 -0300

    iniciado number 25

--- DecisionStructure/number_25.py
+++ DecisionStructure/number_25.py
@@ -5,7 +5,8 @@
     "Esteve no local do crime?"
     "Mora perto da vítima?"
     "Devia para a vítima?"
-    "Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a 
+    "Já trabalhou com a vítima?" 
+    O programa deve no final emitir uma classificação sobre a 
     participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve 
     ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". 
     Caso contrário, ele será classificado como "Inocente".
@@ -21,3 +22,9 @@
     be classified as "Suspect", between 3 and 4 as "Accomplice" and 5 as "Assassin".
     Otherwise, he will be classified as "Innocent".
 """
+
+telefone = input('Telefonou para a vítima? (Sim - Não): ')
+local = input('Esteve no local do crime? (Sim - Não): ')
+mora = input("Mora perto da vítima? (Sim - Não): ")
+divida = input("Devia para a vítima? (Sim - Não): ")
+trabalho = input("Já trabalhou com a vítima? (Sim - Não): ")
