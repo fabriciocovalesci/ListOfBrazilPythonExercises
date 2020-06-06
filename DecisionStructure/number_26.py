1ff8c1936e3d85fbe85f29dc3f0562db7affd3c0
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-05 21:36:52 -0300

    add

---
+++ DecisionStructure/number_26.py
@@ -0,0 +1,26 @@
+"""
+[PT]
+26. Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+    Álcool:
+    até 20 litros, desconto de 3% por litro
+    acima de 20 litros, desconto de 5% por litro
+    Gasolina:
+    até 20 litros, desconto de 4% por litro
+    acima de 20 litros, desconto de 6% por litro Escreva um algoritmo que leia o número de 
+    litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), 
+    calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina 
+    é R$ 2,50 o preço do litro do álcool é R$ 1,90.
+
+
+[EN]
+26. A gas station is selling fuel with the following discount table:
+    Alcohol:
+    up to 20 liters, 3% discount per liter
+    over 20 liters, 5% discount per liter
+    Gasoline:
+    up to 20 liters, 4% discount per liter
+    over 20 liters, 6% discount per liter Write an algorithm that reads the number of
+    liters sold, the type of fuel (coded as follows: A-alcohol, G-gasoline),
+    calculate and print the amount to be paid by the customer knowing that the price of a liter of gasoline
+    is R $ 2.50 the price of a liter of alcohol is R $ 1.90.
+"""
---
+++ DecisionStructure/number_27.py
@@ -0,0 +1,23 @@
+"""
+[PT]
+27. Uma fruteira está vendendo frutas com a seguinte tabela de preços:
+
+                        Até 5 Kg           Acima de 5 Kg
+    Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
+    Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg
+
+Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, 
+receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) 
+de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
+
+[EN]
+27. A fruit tree is selling fruit with the following price list:
+
+                        Up to 5 Kg    Above 5 Kg
+    Strawberry      R$ 2.50 per Kg    R$ 2.20 per Kg
+    Apple           R$ 1.80 per kg    R$ 1.50 per kg
+
+If the customer buys more than 8 kg of fruit or the total purchase value exceeds R $ 25.00,
+you will also receive a 10% discount on this total. Write an algorithm to read the quantity (in Kg)
+of strawberries and the quantity (in Kg) of apples purchased and write the amount to be paid by the customer.
+"""
