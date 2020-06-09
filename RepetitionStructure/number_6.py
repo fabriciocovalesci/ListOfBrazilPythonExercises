39e7ca6708032ca9fb89f6425bee1126bc502e34
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-08 21:32:04 -0300

    add

---
+++ RepetitionStructure/number_6.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+6. Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+[EN]
+6. Make a program that prints the numbers from 1 to 20, one below the other. 
+Then modify the program so that it shows the numbers next to each other.
+"""
