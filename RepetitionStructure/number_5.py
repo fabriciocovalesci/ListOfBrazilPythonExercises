adcdf2a83e1cc60c053d71c4ffa7a9b56783691a
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-08 21:30:43 -0300

    add

---
+++ RepetitionStructure/number_5.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+5. Altere o programa anterior permitindo ao usuário informar as populações e as taxas de 
+crescimento iniciais. Valide a entrada e permita repetir a operação.
+
+[EN]
+5. Change the previous program allowing the user to inform populations and rates of
+initial growth. Validate the entry and allow the operation to be repeated.
+"""
