fb9a920fa02fd3a76adbcec0d8124fccef1107f3
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-11 20:06:41 -0300

    adicionado number 10

---
+++ RepetitionStructure/number_10.py
@@ -0,0 +1,15 @@
+"""
+[PT]
+10. Faça um programa que receba dois números inteiros e gere os números inteiros que estão no 
+intervalo compreendido por eles.
+
+[EN]
+10. Make a program that receives two whole numbers and generates the whole numbers that are in the
+range understood by them.
+"""
+
+import random
+
+number_1 = int(input("Numero 1: "))
+number_2 = int(input("Numero 2: "))
+
