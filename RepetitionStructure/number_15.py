9465bb0ff4c880ff81ffac663f2cba3478f2e5f0
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-06-12 22:33:36 -0300

    add number 15

---
+++ RepetitionStructure/number_15.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+15. A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... 
+Faça um programa capaz de gerar a série até o n−ésimo termo.
+
+[EN]
+The Fibonacci series is formed by the sequence 1,1,2,3,5,8,13,21,34,55, ... 
+Make a program capable of generating the series up to the nth term.
+"""
