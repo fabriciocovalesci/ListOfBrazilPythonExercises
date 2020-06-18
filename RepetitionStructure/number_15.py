fd1032762cdc2e31a4afbcd7a688d39213bbea4d
fabricio <fabcovalesci@gmail.com>
2020-06-17 21:39:20 -0300

    add number15

--- RepetitionStructure/number_15.py
+++ RepetitionStructure/number_15.py
@@ -7,3 +7,19 @@ Faça um programa capaz de gerar a série até o n−ésimo termo.
 The Fibonacci series is formed by the sequence 1,1,2,3,5,8,13,21,34,55, ... 
 Make a program capable of generating the series up to the nth term.
 """
+
+numero = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
+
+
+if (numero==1) or (numero==2):
+    print("1")
+else:
+    count=3
+    while count <= numero:
+        termo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = termo
+        count += 1
+    print(termo)    
