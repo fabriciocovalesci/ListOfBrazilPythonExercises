a8d05f04d8669fc1ac7fe1ee015b4a3a614c6fcb
fabricio <fabcovalesci@gmail.com>
2020-06-17 22:09:24 -0300

    add

---
+++ RepetitionStructure/number_16.py
@@ -0,0 +1,10 @@
+"""
+[PT]
+16. A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... 
+Faça um programa que gere a série até que o valor seja maior que 500.
+
+
+[EN]
+16. The Fibonacci series is formed by the sequence 0,1,1,2,3,5,8,13,21,34,55, ...
+Make a program that manages the series until the value is greater than 500.
+"""
---
+++ RepetitionStructure/number_17.py
@@ -0,0 +1,9 @@
+"""
+[PT]
+17. Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário. 
+Ex.: 5!=5.4.3.2.1=120
+
+[EN]
+17. Make a program that calculates the factorial of an integer provided by the user.
+Ex .: 5! = 5.4.3.2.1 = 120
+"""
