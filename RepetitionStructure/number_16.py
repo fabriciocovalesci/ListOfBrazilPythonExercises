4e6ac259196932e1d06d1f3b2465902ae75bda22
fabricio <fabcovalesci@gmail.com>
2020-06-30 23:56:35 -0300

    Finalizado number 16

--- RepetitionStructure/number_16.py
+++ RepetitionStructure/number_16.py
@@ -8,3 +8,15 @@ Faça um programa que gere a série até que o valor seja maior que 500.
 16. The Fibonacci series is formed by the sequence 0,1,1,2,3,5,8,13,21,34,55, ...
 Make a program that manages the series until the value is greater than 500.
 """
+
+
+
+def fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+        print()
+
+    
+fibonacci(500)
