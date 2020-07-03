949a760cb75de03fe7d8182d10c881461079e710
fabricio <fabcovalesci@gmail.com>
2020-07-02 22:31:20 -0300

    add

--- RepetitionStructure/number_18.py
+++ RepetitionStructure/number_18.py
@@ -1,3 +1,10 @@
 """
+[PT]
+18. Faça um programa que, dado um conjunto de N números, determine o menor valor, 
+o maior valor e a soma dos valores.
 
+[EN]
+18. Make a program that, given a set of N numbers, determines the smallest value,
+the highest value and the sum of the values.
 """
+
